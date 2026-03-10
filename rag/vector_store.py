09f21788df1b189086ce4f5f96d3198fc6601f68
Nikhil <nikhilgautam0216@gmail.com>
2026-03-10 23:05:06 +0530

    added vector store

---
+++ rag/vector_store.py
@@ -0,0 +1,26 @@
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.document_loaders import TextLoader
+
+def build_vector_store():
+
+    loader = TextLoader("rag/recon_knwlg.txt")
+    documents = loader.load()
+
+    splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    docs = splitter.split_documents(documents)
+
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
+    db = Chroma.from_documents(
+        docs,
+        embeddings,
+        persist_directory="rag/chroma_db"
+    )
+
+    db.persist()
+
+    return db
